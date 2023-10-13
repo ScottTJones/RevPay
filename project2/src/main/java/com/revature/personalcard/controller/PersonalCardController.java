2afceedd0f537fb2f69e50afc087afcec2bd7f44
magicman1135 <scottjonesjr89@gmail.com>
2023-10-12 20:21:30 -0700

    Added Card Creation to the controller still unsure on endpoint.

--- project2/src/main/java/com/revature/personalcard/controller/PersonalCardController.java
+++ project2/src/main/java/com/revature/personalcard/controller/PersonalCardController.java
@@ -1,4 +1,34 @@
 package com.revature.personalcard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import com.revature.personalcard.model.PersonalCard;
+import com.revature.personalcard.util.CardUtils;
+import com.revature.personalcard.services.PersonalCardService;
+
+@RestController
 public class PersonalCardController {
+    public PersonalCardController() {
+		super();
+	}
+
+	public PersonalCardController(PersonalCardService personalCardService) {
+		super();
+		this.personalCardService = personalCardService;
+	}
+
+	public PersonalCardService getPersonalCardService() {
+		return personalCardService;
+	}
+
+	@Autowired
+	public void setPersonalCardService(PersonalCardService personalCardService) {
+		this.personalCardService = personalCardService;
+	}
+	
+	@PostMapping("/")
+	public PersonalCard PostPersonal_Card(@RequestBody PersonalCard personalCard) {
+		return personalCardService.createNewCard(personalCard);
+	}
 }
+
